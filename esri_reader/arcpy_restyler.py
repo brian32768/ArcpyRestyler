00d39529e8c346127829aab56c94aa6825e34452
Brian Wilson <bwilson@co.clatsop.or.us>
2018-10-19 17:03:55 -0700

    added lots of code

--- esri_reader/arcpy_restyler.py
+++ esri_reader/arcpy_restyler.py
@@ -12,10 +12,102 @@ import urllib.request
 import json
 
 __version__ = ".001" # Set this to whatever you want or just delete it.
+JSONFORMAT = 'f=json'
 
 class arcpy_script(object):
     def __init__(self):
         return
+
+def describe_feature_service(service):
+    serviceurl = service + '?' + JSONFORMAT
+    with urllib.request.urlopen(serviceurl) as url:
+        rawdata = url.read()
+        jsondata = rawdata.decode()
+        parsed = json.loads(jsondata)
+        #print(json.dumps(parsed, indent=4, sort_keys=True))
+        layers = parsed['layers']
+        docinfo = parsed['documentInfo']
+        print("Title: %s" % docinfo['Title'])
+        sref = parsed["spatialReference"]
+        print("SREF: %s" % sref['wkid'])
+        pass
+
+    for layer in layers:
+        describe_layer(service, layer)
+    return
+
+d_geometry_types = { 'esriGeometryPolygon': 'Polygon', }
+
+def describe_layer(service, layer):
+    #print(layer)
+    name  = layer['name']
+    id    = layer['id']
+    geometry_type = layer['geometryType']
+    layerurl   = "%s/%s/?%s" % (service, id, JSONFORMAT)
+    visibility = layer['defaultVisibility'] == True
+    #other properties: minScale, maxScale, type=Feature Layer
+
+    print("ID: %d %s Layer name: '%s' visible? %s" % (id, d_geometry_types[geometry_type], name, visibility))
+
+    with urllib.request.urlopen(layerurl) as url:
+        rawdata = url.read()
+        jsondata = rawdata.decode()
+        parsed = json.loads(jsondata)
+        #print(json.dumps(parsed, indent=4, sort_keys=True))
+        print("Fields:")
+        oid = None
+        fields = parsed['fields']
+        for field in fields:
+            fieldtype = field['type']
+            if fieldtype == 'esriFieldTypeOID':
+                oid = field
+            #print("  ", field['name'], fieldtype)
+
+        sref         = parsed["sourceSpatialReference"]['wkid']
+        styleinfo    = parsed['drawingInfo']
+        labelinginfo = styleinfo['labelingInfo']
+        renderer     = styleinfo['renderer']
+        opacity   = 1 - styleinfo['transparency']
+        describe_label_style(labelinginfo)
+        describe_renderer(renderer)
+        pass
+    return
+
+def describe_label_style(style):
+    if not style: 
+        return
+    print("labelingInfo ----\n", json.dumps(style, indent=4, sort_keys=True))
+    label  = style['label']
+    symbol = style['symbol']
+    describe_color(symbol["color"])
+    try:
+        outline = symbol["outline"]
+        print("outline: width %d" % outline['width'])
+        describe_color(outline["color"])
+    except:
+        pass
+    return
+
+def describe_renderer(styles):
+    #print("renderer  -------\n", json.dumps(style, indent=4, sort_keys=True))
+    for style in styles:
+        describe_style(style)
+    return
+
+def describe_style(style):
+    print("label: '%s' '%s'" % (style['label'], style['value']))
+    symbol = style['symbol']
+    describe_color(symbol['color'])
+    try:
+        outline = symbol['outline']
+        print("outline: width %d" % outline['width'])
+        describe_color(outline['color'])
+    except:
+        pass
+    return
+
+def describe_color(color):
+    print("color: %d %d %d %d" % color)
 
 # ===================================================================================
 if __name__ == "__main__":
@@ -31,14 +123,17 @@ if __name__ == "__main__":
     # or you can set up this code to run standalone from command line
 
     #s = arcpy_script()
+    all_services    = "https://cc-gis.clatsop.co.clatsop.or.us/arcgis/rest/services"
+    # for folder in folders:
+    #    describe_service(service + '/' + folder)
+    # for layerservice in services:
+    #     describe_service(service + '/' + layerservice)
 
-    service = "https://cc-gis.clatsop.co.clatsop.or.us/arcgis/rest/services/Assessment_and_Taxation/Taxlots_3857/FeatureServer/1/?f=json"
-    with urllib.request.urlopen(service) as url:
-        rawdata = url.read()
-        jsondata = rawdata.decode()
-        parsed = json.loads(jsondata)
-        print(json.dumps(parsed, indent=4, sort_keys=True))
+    taxlots_vectortiles    = all_services + "/Hosted/WM_taxlots"
+    taxlots_featureservice = all_services + "/Assessment_and_Taxation/Taxlots_3857/FeatureServer/"
+    zoning_featureservice  = all_services + "/Zoning_3857/FeatureServer/"
 
-    pass
+    for service in [zoning_featureservice]:
+        describe_feature_service(service)
 
 # That's all!
